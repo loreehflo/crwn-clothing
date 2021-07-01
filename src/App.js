3ac3efa8cc5a051d47bd649374b2053de31b034f
Lorena Nolasco <lorena.nolascoflores@gmail.com>
2021-07-01 17:03:37 -0500

    Header Component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx'
 
 const HatsPage = () => (
   <div>
@@ -15,9 +17,10 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
