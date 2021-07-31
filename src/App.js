534b11c85a2691250d5235a3ef948d15c2457ba4
Lorena Nolasco <lorena.nolascoflores@gmail.com>
2021-07-31 09:43:41 -0500

    added addItem and removeItem functionality to checkout item component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SingInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component.jsx'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 const HatsPage = () => (
   <div>
@@ -52,15 +56,16 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route exaact path='/signinandsignup' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SingInAndSignUp />)} />
+          <Route extact path='/checkout' component={CheckoutPage} />
+          <Route exact path='/signinandsignup' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SingInAndSignUp />)} />
         </Switch>
       </div >
     );
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUSer
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
